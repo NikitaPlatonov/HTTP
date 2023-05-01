2759e078c90602a5b0e92f3af099a2d183b4c3ff
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-05-01 11:18:10 +0300

    I wrote a method readJsonFormatToJavaObject and completed the work

--- src/main/java/ru/netology/CatFacts.java
+++ src/main/java/ru/netology/CatFacts.java
@@ -3,29 +3,26 @@ package ru.netology;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class CatFacts {
-    private final int id;
+    private final String id;
     private final String text;
+    private final String type;
     private final String user;
     private final int upvotes;
 
-    public CatFacts(
-            @JsonProperty("id") int id,
-            @JsonProperty("text") String text,
-            @JsonProperty("user") String user,
-            @JsonProperty("upvotes") int upvotes
-    ) {
+    public CatFacts(@JsonProperty("id") String id, @JsonProperty("text") String text, @JsonProperty("type") String type, @JsonProperty("user") String user, @JsonProperty("upvotes") int upvotes) {
         this.id = id;
         this.text = text;
+        this.type = type;
         this.user = user;
         this.upvotes = upvotes;
     }
 
     @Override
     public String toString() {
-        return "CatFacts" +
-                "\n id: " + id +
-                "\n text: " + text +
-                "\n user: " + user +
-                "\n upvotes: " + upvotes;
+        return "CatFacts" + "\n id: " + id + "\n text: " + text + "\n type: " + type + "\n user: " + user + "\n upvotes: " + upvotes;
+    }
+
+    public int getUpvotes() {
+        return upvotes;
     }
 }
